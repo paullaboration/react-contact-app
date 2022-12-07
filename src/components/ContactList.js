391e075fded50a2ea2566fab46749c618906b1a0
paullaboration <juleskwoon@gmail.com>
2022-12-07 20:39:13 +0800

    ContactList added

---
+++ src/components/ContactList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactList =(props) => {
+    console.log(props);
+    return (
+        <div className="ui celled list">
+            Contact List
+        </div>
+    )
+}
+
+export default ContactList;
