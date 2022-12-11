efcfe51138ad129fe77bb4d3076b3e193819d2b0
paullaboration <juleskwoon@gmail.com>
2022-12-11 17:36:33 +0800

    remove contact function added

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { uuid } from 'uuidv4';
 import './App.css';
 import Header from "./Header";
 import AddContact from './AddContact';
@@ -10,8 +11,16 @@ function App() {
 
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, {id: uuid(), ...contact }]);
   };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    
+    setContacts(newContactList);
+  }
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
@@ -21,7 +30,7 @@ function App() {
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} />   
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />   
     </div>
   );
 }
