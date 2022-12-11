bb15f577ecedd3bf31d5d9df2c3a5f4a2d941715
paullaboration <juleskwoon@gmail.com>
2022-12-11 08:42:07 +0800

    local storage added

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import Header from "./Header";
 import AddContact from './AddContact';
 import ContactList from "./ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact]);
-    
   };
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div className="ui container">
       <Header />
